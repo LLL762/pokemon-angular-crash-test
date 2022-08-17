d4fbeb327815525cfaba3eccf4a351c7d4782594
LLL762 <lamirallaurent@gmail.com>
2022-08-17 15:39:15 +0200

    second commit.

--- src/app/pokemon/pkm-search/pkm-search.component.ts
+++ src/app/pokemon/pkm-search/pkm-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { debounceTime, distinctUntilChanged, filter, Observable, Subject, switchMap } from 'rxjs';
 import { PkmService } from '../pkm.service';
@@ -11,12 +11,16 @@ import { Pokemon } from '../pokemon';
 })
 export class PkmSearchComponent implements OnInit {
 
+
+  @ViewChild('searchBox') searchBox: ElementRef;
+  @ViewChild('searchBar') searchBar: ElementRef;
+
   //{.."a".."ab"..."abz"...}
   searchTerms = new Subject<string>();
   //{...pokemonLisdt(a)..pokemonList(ab)..}
   pokemons$: Observable<Pokemon[]>;
 
-  constructor(private pkmService: PkmService, private router: Router) { }
+  constructor(private pkmService: PkmService, private router: Router, private renderer: Renderer2) { }
 
   ngOnInit(): void {
     this.pokemons$ = this.searchTerms.pipe(
@@ -31,6 +35,19 @@ export class PkmSearchComponent implements OnInit {
   search(term: string) {
     this.searchTerms.next(term);
   }
+
+
+  selectSearchBox(): void {
+    this.renderer.selectRootElement(this.searchBox["nativeElement"]).focus();
+  }
+
+  onFocusSearchBox(): void {
+    this.renderer.addClass(this.searchBar["nativeElement"], 'outline');
+  }
+  onFocusOutSearchBox(): void {
+    this.renderer.removeClass(this.searchBar["nativeElement"], 'outline');
+  }
+
 
   goToDetail(pokemon: Pokemon) {
     const url = ['/pokemon', pokemon.id];
